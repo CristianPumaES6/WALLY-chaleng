63d98c7d54a14a8fe5fe7714e8ad48c16e3b452e
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2022-02-16 20:35:29 -0500

    Cambios de la mañana

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Chaleng WALLY';
+  public listUser = [];
+
+  constructor(
+    private _userService: AppService,
+  ) { }
+
+
+
+  public async ClickRegistrarUsuario() {
+
+    return await Promise.resolve(true).then(
+      result => {
+
+        let createUser = {
+          name: 'Juan',
+          dni: '77871220'
+        };
+
+        return this._userService.Create(createUser).pipe().toPromise()
+      }
+    )
+  }
+
+
+
+  public async ClickActualizarUsuario() {
+
+    return await Promise.resolve(true).then(
+      result => {
+
+        let updateUser = {
+          name: 'Juan',
+          dni: '77871220'
+        };
+
+        return this._userService.Update(updateUser).pipe().toPromise()
+      }
+    )
+  }
+
+
+  public async ClickListarUsurio() {
+
+    return await Promise.resolve(true).then(
+      result => {
+
+        let updateUser = {
+          name: 'Juan',
+          dni: '77871220'
+        };
+
+        return this._userService.Get(1).pipe().toPromise()
+      }
+    ).then(
+      result => {
+        this.listUser = result;
+      }
+    )
+  }
+
 }
